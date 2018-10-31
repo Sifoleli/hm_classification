26cad482bc526c536d84a694800fdd425435dc00
neto_alcides <alcides.carlos@yahoo.com.br>
2018-10-31 18:00:37 -0400

    atualizar codigo de modelo

--- munge/05-Modelo.R
+++ munge/05-Modelo.R
@@ -1,24 +1,5 @@
 info( logger, "HM_CLASSIFICATION::modelo" )
 
-###### modelo 1
-# network <- keras_model_sequential() %>%
-#   layer_dense( units = 512,
-#                activation = "relu",
-#                input_shape = c(86 * 85) ) %>%
-#   layer_dense( units = 2,
-#                activation = "softmax" )
-# 
-# # metodo de treinamento
-# opt <- optimizer_rmsprop(lr = 0.0001, decay = 1e-6)
-# 
-# network %>% compile(
-#   loss = "categorical_crossentropy",
-#   optimizer = opt,
-#   metrics = "accuracy"
-# )
-
-
-
 network <- keras_model_sequential() %>%
   layer_dense( units = 512,
                activation = "relu",
